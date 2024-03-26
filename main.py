d65e5ddd223e0f156e5be35246a39734591b504f
Lisa3483 <liz.shtodina@mail.ru>
2024-03-26 21:34:34 +0300

    созданно окно регестрации

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+import random
+import string
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Укажите SMTP-сервер для отправки писем
+app.config['MAIL_PORT'] = 587  # Порт сервера
+app.config['MAIL_USE_TLS'] = True  # Использовать TLS
+app.config['MAIL_USERNAME'] = 'egorovicegorka87@gmail.com'  # Ваш email для отправки писем
+app.config['MAIL_PASSWORD'] = 'kfwz tqrt ejgb kcqq'  # Пароль от вашего email
+mail = Mail(app)
+
+
+@app.route('/')
+def index(flagpass=0):
+    param = {}
+    param['flagpass'] = flagpass
+    return render_template('index.html', **param)
+
+
+def generate_code():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+
+    if password != confirm_password:
+        return index(flagpass=1)
+
+    verification_code = generate_code()
+
+    msg = Message('Код подтверждения', sender='your-email@example.com', recipients=[email])
+    msg.body = f'Ваш код подтверждения: {verification_code}'
+    mail.send(msg)
+
+    # Добавьте код для сохранения данных пользователя и кода подтверждения в базе данных или других действий
+    return f'Код подтверждения отправлен на {email}.'
+
+
+def myFunction():
+    print(1)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
