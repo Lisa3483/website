0fadf0616e31b4847af6454b90f13f3bf76b7c42
ivanignatov2 <ivan.ignatov@yandex.ru>
2024-04-15 22:33:25 +0300

    :(

--- cases.py
+++ cases.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import random
 import os
-
+from requests import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,65 +13,142 @@ def index():
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flask App</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style>
+        #container {
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        #getDataBtn1 {
+            margin-top: 20px; /* Задаем отступ сверху */
+        }
+    </style>
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
 </head>
 <body>
-    <div id="container" style="text-align:center;">
-        <div id="state1">
-            <button id="getDataBtn1">Открой кейс 1</button>
-        </div>
-        <div id="state2" style="display: none;">
-            <button id="getDataBtn2">Открой кейс 2</button>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Главное меню <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Кейсы</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Профиль</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Кошелек</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Контакты</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    <img src="pprofil.png">
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Регистрация</a>
+                    <a class="dropdown-item" href="#">Авторизация</a>
+                </div>
+            </li>
+        </ul>
     </div>
+</nav>
 
-    <button id="changeStateBtn">Сменить состояние первой кнопки</button>
+<div id="container">
+    <div id="state1">
+        <button id="getDataBtn1">Открой кейс 1</button>
+    </div>
+    <div id="state2" style="display: none;">
+        <button id="getDataBtn2">Открой кейс 2</button>
+    </div>
+</div>
 
-    <script>
-        $(document).ready(function(){
-            var currentState = 1; // Изначально сайт находится в первом состоянии
-            var currentCase = 1; // Текущий номер кейса
+<button id="changeStateBtn">Сменить состояние первой кнопки</button>
 
-            $('#getDataBtn1').click(function(){
-                if (currentState === 1) {
-                    // Выполнить действия для первого состояния
-                    $.ajax({
-                        url: '/get_data',
-                        type: 'GET',
-                        success: function(response){
-                            console.log(response);
-                            alert(JSON.stringify(response));
-                        },
-                        error: function(error){
-                            console.log(error);
-                        }
-                    });
-                }
-            });
-
-            $('#getDataBtn2').click(function(){
-                if (currentState === 2) {
-                    // Выполнить действия для второго состояния
-                    alert("Another Action performed!");
-                }
-            });
-
-            // Логика для смены состояния первой кнопки
-            $('#changeStateBtn').click(function(){
-                if (currentCase <= 3) {
-                    $('#getDataBtn1').text('Открой кейс ' + currentCase);
-                    currentCase++;
-                } else {
-                    currentCase = 1;
-                }
-            });
+<script>
+$('#getDataBtn1').click(function(){
+    if (currentState === 1) {
+        $.ajax({
+            url: '/get_data',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'case_number': 1 }),
+            success: function(response){
+                console.log(response);
+                alert(response.result);
+            },
+            error: function(error){
+                console.log(error);
+            }
         });
-    </script>
+    }
+});
+
+$('#getDataBtn2').click(function(){
+    if (currentState === 2) {
+        // Выполнить действия для второго состояния
+        $.ajax({
+            url: '/get_data',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'case_number': 2 }),
+            success: function(response){
+                console.log(response);
+                alert(response.result);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    }
+});
+
+$('#getDataBtn3').click(function(){
+    if (currentState === 3) {
+        $.ajax({
+            url: '/get_data',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'case_number': 3 }),
+            success: function(response){
+                console.log(response);
+                alert(response.result);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    }
+});
+
+</script>
 </body>
 </html>
 '''
 
-@app.route('/get_data', methods=['GET'])
+@app.route('/get_data', methods=['POST'])
 def get_data():
+    name = request.form['name']
+    age = request.form['age']
+    email = request.form['email']
     data = [{'dogs': ('', '', 30)}, {'cats': ('', '', 25)}, {'hedgehogs': (0.05, 10000, 'hedgehog1.png')}]
     data_2 = random.choice(data)
     return jsonify(data_2)
--- test(ничего важного).py
+++ test(ничего важного).py
@@ -1,14 +1,147 @@
-import requests
-def main():
-    url = "http://127.0.0.1:5000"  # Замените на URL вашего JSON-файла
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Проверка на ошибки HTTP
-        json_data = response.json()  # Преобразование ответа в JSON формат
-        print(json_data)  # Вывод JSON-данных
-    except requests.exceptions.RequestException as e:
-        print("Ошибка при выполнении запроса:", e)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flask App</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style>
+        #container {
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
 
-if __name__ == "__main__":
-    while True:
-        main()
+        #getDataBtn1 {
+            margin-top: 20px; /* Задаем отступ сверху */
+        }
+    </style>
+    <link rel="stylesheet"
+          href="https://stackpathstackpathstackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Главное меню <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Кейсы</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Профиль</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Кошелек</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Контонтонтакты</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    <img src="pprofil.png">
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Регистрация</a>
+                    <a class="dropdown-item" href="#">Авторизация</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div id="container">
+    <div id="state1">
+        <button id="getDataBtn1">Открой кейс 1</button>
+    </div>
+    <div id="state2" style="display: none;">
+        <button id="getDataBtn2">Открой кейс 2</button>
+    </div>
+</div>
+
+<button id="changeStateBtn">Сменить состояние первой кнопки</button>
+
+<script>
+// Изначальное состояние первой кнопки
+var currentState = 1;
+
+$('#getDataBtn1').click(function(){
+    if (currentState === 1) {
+        // Выполнить действия для первого состояния
+        $.ajax({
+            url: '/get_data',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'case_number': 1 }),
+            success: function(response){
+                console.log(response);
+                alert(response.result);
+            },
+            error error error: function(error){
+                console.log(error);
+            }
+        });
+    }
+});
+
+$('#getDataBtn2').click(function(){
+    if (currentState === 2) {
+        // Выполнить действия для второго состояния
+        $.ajax({
+            url: '/get_data',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'case_number': 2 }),
+            success: function(response){
+                console.log(response);
+                alert(response.result);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    }
+});
+
+$('#getDataBtn3').click(function(){
+    if (currentState === 3) {
+        // Выполнить действия для третьего состояния
+        $.ajax({
+            url: '/get_data',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'case_number': 3 }),
+            success: function(response){
+                console.log(response);
+                alert(response.result);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    }
+});
+
+// Обработчик для кнопки смены состояния первой кнопки
+$('#changeStateBtn').click(function(){
+    currentState = (currentState === 1) ? 2 : 1; // Меняем состояние на противоположное
+    alert("Состояние первой кнопки изменено на " + currentState);
+});
+
+</scriptscriptscript>
+</body>
+</html>
